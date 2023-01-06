98b03ab2edcb99afb4121ee1d18e3adf1812205a
thehxdev <xclimax1324@gmail.com>
2023-01-06 22:42:58 +0330

    updated

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # config-link
 
-1. Put all of your xray/v2ray configurations to `clash.yaml` (For Clash) and `surfboard.conf` (For Surfboard)
+1. Clone this repository to `/root/`.
+1. Put `clash.yaml`, `surfboard.conf` and `xray.txt` files (With exact same name) in `/root/config-link/` (Repository directory).
 1. Run the app with `./app` command.
 1. Server will start on port `3000`.
 1. Then import files to Clash/Surfboard app:
@@ -11,4 +12,7 @@ http://SERVER_IP:3000/clash.yaml
 
 # Surfboard
 http://SERVER_IP:3000/surfboard.conf
+
+# Any V2ray/Xray client that supports subscription
+http://SERVER_IP:3000/xray.txt
 ```
--- main.go
+++ main.go
@@ -3,23 +3,23 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func main() {
-	app := fiber.New()
+    app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hi")
-	})
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello From Fiber!")
+    })
 
-	app.Get("/surfboard.conf", func(c *fiber.Ctx) error {
-		return c.SendFile("/root/config-link/surfboard.conf")
-	})
+    app.Get("/surfboard.conf", func(c *fiber.Ctx) error {
+        return c.SendFile("/root/config-link/surfboard.conf")
+    })
 
-	app.Get("/clash.yaml", func(c *fiber.Ctx) error {
-		return c.SendFile("/root/config-link/clash.yaml")
-	})
+    app.Get("/clash.yaml", func(c *fiber.Ctx) error {
+        return c.SendFile("/root/config-link/clash.yaml")
+    })
 
-	app.Get("/xray.txt", func(c *fiber.Ctx) error {
-		return c.SendFile("/root/config-link/xray.txt")
-	})
+    app.Get("/xray.txt", func(c *fiber.Ctx) error {
+        return c.SendFile("/root/config-link/xray.txt")
+    })
 
-	app.Listen(":3000")
+    app.Listen(":3000")
 }
